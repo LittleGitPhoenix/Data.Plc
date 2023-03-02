2ccfe51c5331a69046a9a9cf32bd008253b29817
Little Phoenix <git@little-phoenix.de>
2023-03-02 10:27:24 +0100

    Implemented explicit usings and file scoped namespaces in Plc.AgLink.Demo.Test project.

--- src/Plc.AgLink.Demo.Test/ReadTest.cs
+++ src/Plc.AgLink.Demo.Test/ReadTest.cs
@@ -1,32 +1,29 @@
-using System;
 using System.Net.NetworkInformation;
-using NUnit.Framework;
 using Phoenix.Data.Plc.Implementation.Test;
 
-namespace Phoenix.Data.Plc.AgLink.Test
+namespace Phoenix.Data.Plc.AgLink.Test;
+
+[TestFixture]
+public sealed class ImplementationReadTest : ImplementationReadTest<AgLinkPlc>
 {
-	[TestFixture]
-	public sealed class ImplementationReadTest : ImplementationReadTest<AgLinkPlc>
+	private static string Host = "PLC1518";
+
+	public ImplementationReadTest()
+		: base
+		(
+			new DemoAgLinkPlc(new AgLinkPlcConnectionData(0, ImplementationReadTest.Host, 0, 0))
+		) { }
+
+	/// <inheritdoc />
+	protected override void CheckConnectivity()
 	{
-		private static string Host = "PLC1518";
-
-		public ImplementationReadTest()
-			: base
-			(
-				new DemoAgLinkPlc(new AgLinkPlcConnectionData(0, ImplementationReadTest.Host, 0, 0))
-			) { }
-
-		/// <inheritdoc />
-		protected override void CheckConnectivity()
+		var ping = new Ping();
+		PingReply reply = null;
+		try
 		{
-			var ping = new Ping();
-			PingReply reply = null;
-			try
-			{
-				reply = ping.Send(Host, 1000);
-			}
-			catch { /* ignore */ }
-			if (reply is null || reply.Status != IPStatus.Success) Assert.Ignore($"Establishing a connection to '{base.Plc}' failed. This implementation test will not be executed, because it needs special hardware which seems to be unavailable.");
+			reply = ping.Send(Host, 1000);
 		}
+		catch { /* ignore */ }
+		if (reply is null || reply.Status != IPStatus.Success) Assert.Ignore($"Establishing a connection to '{base.Plc}' failed. This implementation test will not be executed, because it needs special hardware which seems to be unavailable.");
 	}
 }
---
+++ src/Plc.AgLink.Demo.Test/⬒/Usings.cs
@@ -0,0 +1,10 @@
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+global using AutoFixture;
+global using AutoFixture.AutoMoq;
+global using Moq;
+global using NUnit.Framework;
