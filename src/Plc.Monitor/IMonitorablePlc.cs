523d9ddd47a7b9ae9dfebf50600c2b20dd225abf
Little Phoenix <git@little-phoenix.de>
2023-03-02 10:09:11 +0100

    Implemented explicit usings and file scoped namespaces in Plc.Monitor project.

--- src/Plc.Monitor/IMonitorablePlc.cs
+++ src/Plc.Monitor/IMonitorablePlc.cs
@@ -4,10 +4,9 @@
 
 using Phoenix.Data.Plc.Items;
 
-namespace Phoenix.Data.Plc.Monitor
-{
-	/// <summary>
-	/// Interface for an <see cref="IPlc"/> that can be used to monitor <see cref="IPlcItem"/>s.
-	/// </summary>
-	public interface IMonitorablePlc : IPlc, IPlcMonitor { }
-}
+namespace Phoenix.Data.Plc.Monitor;
+
+/// <summary>
+/// Interface for an <see cref="IPlc"/> that can be used to monitor <see cref="IPlcItem"/>s.
+/// </summary>
+public interface IMonitorablePlc : IPlc, IPlcMonitor { }
--- src/Plc.Monitor/IPlcMonitor.cs
+++ src/Plc.Monitor/IPlcMonitor.cs
@@ -2,36 +2,34 @@
 //! This file is subject to the terms and conditions defined in file 'LICENSE.md', which is part of this source code package.
 #endregion
 
-using System;
 using Phoenix.Data.Plc.Items;
 
-namespace Phoenix.Data.Plc.Monitor
+namespace Phoenix.Data.Plc.Monitor;
+
+/// <summary>
+/// Interface for all plc monitor classes.
+/// </summary>
+public interface IPlcMonitor : IDisposable
 {
 	/// <summary>
-	/// Interface for all plc monitor classes.
+	/// Starts monitoring all available <see cref="IPlcItem"/>s.
 	/// </summary>
-	public interface IPlcMonitor : IDisposable
-	{
-		/// <summary>
-		/// Starts monitoring all available <see cref="IPlcItem"/>s.
-		/// </summary>
-		void Start();
+	void Start();
 
-		/// <summary>
-		/// Stops monitoring all available <see cref="IPlcItem"/>s.
-		/// </summary>
-		void Stop();
+	/// <summary>
+	/// Stops monitoring all available <see cref="IPlcItem"/>s.
+	/// </summary>
+	void Stop();
 
-		/// <summary>
-		/// Starts monitoring the <paramref name="plcItem"/>.
-		/// </summary>
-		/// <param name="plcItem"> The <see cref="IPlcItem"/> to monitor. </param>
-		void MonitorItem(IPlcItem plcItem);
+	/// <summary>
+	/// Starts monitoring the <paramref name="plcItem"/>.
+	/// </summary>
+	/// <param name="plcItem"> The <see cref="IPlcItem"/> to monitor. </param>
+	void MonitorItem(IPlcItem plcItem);
 
-		/// <summary>
-		/// Stops monitoring the <paramref name="plcItem"/>
-		/// </summary>
-		/// <param name="plcItem"> The <see cref="IPlcItem"/> to stop monitoring. </param>
-		void UnMonitorItem(IPlcItem plcItem);
-	}
+	/// <summary>
+	/// Stops monitoring the <paramref name="plcItem"/>
+	/// </summary>
+	/// <param name="plcItem"> The <see cref="IPlcItem"/> to stop monitoring. </param>
+	void UnMonitorItem(IPlcItem plcItem);
 }
