fb995958d35bd96f3272645964fc8c838a32b67d
Little Phoenix <git@little-phoenix.de>
2023-03-02 10:08:11 +0100

    Implemented explicit usings and file scoped namespaces in Plc.Mock project.

--- src/Plc.Mock/DataRegion.cs
+++ src/Plc.Mock/DataRegion.cs
@@ -2,102 +2,100 @@
 //! This file is subject to the terms and conditions defined in file 'LICENSE.md', which is part of this source code package.
 #endregion
 
-using System.Linq;
 using Phoenix.Data.Plc.Items;
 
-namespace Phoenix.Data.Plc.Mock
+namespace Phoenix.Data.Plc.Mock;
+
+/// <summary>
+/// Represent a region of data.
+/// </summary>
+internal class DataRegion
 {
-	/// <summary>
-	/// Represent a region of data.
-	/// </summary>
-	internal class DataRegion
-	{
-		#region Delegates / Events
-		#endregion
+	#region Delegates / Events
+	#endregion
 
-		#region Constants
-		#endregion
+	#region Constants
+	#endregion
 
-		#region Fields
+	#region Fields
 
-		private readonly object _lock;
+	private readonly object _lock;
 		
-		#endregion
+	#endregion
 
-		#region Properties
+	#region Properties
 
-		internal BitCollection BitCollection { get; }
+	internal BitCollection BitCollection { get; }
 
-		#endregion
+	#endregion
 
-		#region (De)Constructors
+	#region (De)Constructors
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		internal DataRegion()
-			: this(new byte[0])
-		{ }
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	internal DataRegion()
+		: this(new byte[0])
+	{ }
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="initialData"> Initial data for this region. </param>
-		internal DataRegion(byte[] initialData)
-		{
-			// Save parameters.
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="initialData"> Initial data for this region. </param>
+	internal DataRegion(byte[] initialData)
+	{
+		// Save parameters.
 
-			// Initialize fields.
-			_lock = new object();
-			this.BitCollection = new BitCollection(false, initialData);
-		}
-
-		#endregion
-
-		#region Methods
-
-		/// <summary>
-		/// Tries to extend the <see cref="DataRegion"/> to the <paramref name="newSize"/>.
-		/// </summary>
-		/// <param name="newSize"> The new size of the region. </param>
-		internal void TryExtend(uint newSize)
-		{
-			lock (_lock)
-			{
-				// Only extended the bit collection.
-				if (this.BitCollection.Length < newSize) this.BitCollection.Resize(newSize);
-			}
-		}
-
-		/// <summary>
-		/// Reads the data from <paramref name="start"/> with <paramref name="length"/>.
-		/// </summary>
-		/// <param name="start"> The start index from where to read the data. </param>
-		/// <param name="length"> The length to read. </param>
-		/// <returns> The data as <see cref="bool"/> array. </returns>
-		internal bool[] Read(int start, int length)
-		{
-			lock (_lock)
-			{
-				return ((bool[]) this.BitCollection).Skip(start).Take(length).ToArray();
-			}
-		}
-
-		/// <summary>
-		/// Writes the <paramref name="data"/> beginning at <paramref name="start"/>.
-		/// </summary>
-		/// <param name="data"> The <see cref="BitCollection"/> to write. </param>
-		/// <param name="start"> The starting position. </param>
-		/// <returns> <c>True</c> on success, otherwise <c>False</c>. </returns>
-		internal bool Write(BitCollection data, uint start)
-		{
-			lock (_lock)
-			{
-				this.BitCollection.TransferValuesFrom(data, start);
-			}
-			return true;
-		}
-
-		#endregion
+		// Initialize fields.
+		_lock = new object();
+		this.BitCollection = new BitCollection(false, initialData);
 	}
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Tries to extend the <see cref="DataRegion"/> to the <paramref name="newSize"/>.
+	/// </summary>
+	/// <param name="newSize"> The new size of the region. </param>
+	internal void TryExtend(uint newSize)
+	{
+		lock (_lock)
+		{
+			// Only extended the bit collection.
+			if (this.BitCollection.Length < newSize) this.BitCollection.Resize(newSize);
+		}
+	}
+
+	/// <summary>
+	/// Reads the data from <paramref name="start"/> with <paramref name="length"/>.
+	/// </summary>
+	/// <param name="start"> The start index from where to read the data. </param>
+	/// <param name="length"> The length to read. </param>
+	/// <returns> The data as <see cref="bool"/> array. </returns>
+	internal bool[] Read(int start, int length)
+	{
+		lock (_lock)
+		{
+			return ((bool[]) this.BitCollection).Skip(start).Take(length).ToArray();
+		}
+	}
+
+	/// <summary>
+	/// Writes the <paramref name="data"/> beginning at <paramref name="start"/>.
+	/// </summary>
+	/// <param name="data"> The <see cref="BitCollection"/> to write. </param>
+	/// <param name="start"> The starting position. </param>
+	/// <returns> <c>True</c> on success, otherwise <c>False</c>. </returns>
+	internal bool Write(BitCollection data, uint start)
+	{
+		lock (_lock)
+		{
+			this.BitCollection.TransferValuesFrom(data, start);
+		}
+		return true;
+	}
+
+	#endregion
 }
